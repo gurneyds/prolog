731cf87ef200f647ee0d0282eaf5b6c01fc39a67
David Gurney <gurneyds@familysearch.org>
2018-04-09 07:25:36 -0600

    Starting to work, but we actually move to a barrier

--- solve.pl
+++ solve.pl
@@ -35,45 +35,52 @@
 move(_, List, NewList, Row, Col, GoalRow, GoalColumn) :-
   Row =:= GoalRow,
   Col =:= GoalColumn,
+  write('Goal found!!!!'),
   append(List, [[Row,Col]], NewList).
 
-% Found an open square
+% Found an open square, move down
 move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
   maze(Maze, Row, Col, Status),
   Status == open,
   write('Row = '), write(Row),
   write(' Col = '), write(Col),
-  write(' Found open'), nl,
+  write(' Found open'),
   append(List, [[Row,Col]], AppendedList),
-  NextCol is Col + 1,
-  move(Maze, AppendedList, NewList, Row, NextCol, GoalRow, GoalColumn).
+  write(' Moving down'), nl,
+  NextRow is Row + 1,
+  move(Maze, AppendedList, NewList, NextRow, Col, GoalRow, GoalColumn).
 
+% Found an open square, move right
 move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
     maze(Maze, Row, Col, Status),
     Status == open,
     write('Row = '), write(Row),
     write(' Col = '), write(Col),
-    write(' Found open'), nl,
+    write(' Found open'),
     append(List, [[Row,Col]], AppendedList),
-    NextRow is Row + 1,
-    move(Maze, AppendedList, NewList, NextRow, Col, GoalRow, GoalColumn).
+    write(' moving right'), nl,
+    NextCol is Col + 1,
+    move(Maze, AppendedList, NewList, Row, NextCol, GoalRow, GoalColumn).
 
 
 % Hit a barrier, move right
 move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
   hitBarrier(Maze, Row, Col),
   C2 is Col + 1,
+  write(' moving right'), nl,
   move(Maze, List, NewList, Row, C2, GoalRow, GoalColumn).
 
 % Hit a barrier, move down
 move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
   hitBarrier(Maze, Row, Col),
   R2 is Row + 1,
+  write(' moving down'), nl,
   move(Maze, List, NewList, R2, Col, GoalRow, GoalColumn).
 
 % Hit a barrier, move left
 move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
   hitBarrier(Maze, Row, Col),
+  write(' moving left'), nl,
   C2 is Col - 1,
   move(Maze, List, NewList, Row, C2, GoalRow, GoalColumn).
 
@@ -81,13 +88,17 @@ move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
 move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
   hitBarrier(Maze, Row, Col),
   R2 is Row - 1,
+  write(' moving up'), nl,
   move(Maze, List, NewList, R2, Col, GoalRow, GoalColumn).
 
 %----------------------
 % Helper predicate, return true if barrier is at Row, Col
 hitBarrier(Maze, Row, Col) :-
   maze(Maze, Row, Col, Status),
-  Status == barrier.
+  Status == barrier,
+  write('Row = '), write(Row),
+  write(' Col = '), write(Col),
+  write(' Found barrier').
 
 %-----------------------------------
 printCell(Maze, _, Row, Column) :-
@@ -167,4 +178,5 @@ printMaze(List, Maze) :-
 solve(Maze) :-
 	mazeSize(Maze, GoalRow, GoalColumn),
 	move(Maze, [], NewList, 1, 1, GoalRow, GoalColumn),
-	write(NewList).
+	write(NewList), nl,
+  printMaze(NewList, Maze).
