769f7f3497eb701a5b4d5268f0b02da4db964630
David Gurney <gurneyds@familysearch.org>
2018-04-08 21:23:25 -0600

    latest

--- krista.pl
+++ krista.pl
@@ -3,7 +3,27 @@
 % the approach.
 
 % Try a move in an "Up" direction, assumes Row and Column are bound.
-%try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column - 1.
+
+% Move up
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row - 1,
+%  NextCol is Col.
+
+% Move down
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row + 1,
+%  NextCol is Col - 1.
+
+% Move right
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row,
+%  NextCol is Col + 1.
+
+% Move left
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row,
+%  NextCol is Col - 1.
+
 
 % move(Maze, List, NewList, Row, Column, GoalRow, GoalColumn) - moves,
 %   and keep on moving until the GoalRow and GoalColumn coordinates
@@ -11,79 +31,122 @@
 %   that the current move is not one previously made), NewList will be
 %   bound to the list of successful moves in a solved maze.
 
-%
-%   Recursive case still needed.
+% Arrived at goal
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  Row =:= GoalRow,
+  Col =:= GoalColumn,
+  append(List, [[Row,Col]], NewList).
 
-% printCell(Maze, List, Row, Column) - helper goal for printMaze, printCell
-%   prints a single cell in the maze.
-%
-%   Print a barrier.
+% Found an open square
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  maze(Maze, Row, Col, Status),
+  Status == open,
+  append(List, [[Row,Col]], AppendedList),
+  NextCol is Col + 1,
+  move(Maze, AppendedList, NewList, Row, NextCol, GoalRow, GoalColumn).
+
+
+% Hit a barrier, move right
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  C2 is Col + 1,
+  move(Maze, List, NewList, Row, C2, GoalRow, GoalColumn).
+
+% Hit a barrier, move down
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  R2 is Row + 1,
+  move(Maze, List, NewList, R2, Col, GoalRow, GoalColumn).
+
+% Hit a barrier, move left
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  C2 is Col - 1,
+  move(Maze, List, NewList, Row, C2, GoalRow, GoalColumn).
+
+% Hit a barrier, move up
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  R2 is Row - 1,
+  move(Maze, List, NewList, R2, Col, GoalRow, GoalColumn).
+
+%----------------------
+solve(Maze) :-
+	mazeSize(Maze, GoalRow, GoalColumn),
+	move(Maze, [], NewList, 1, 1, GoalRow, GoalColumn),
+	write(NewList).
+
+
+% Helper predicate, return true if barrier is at Row, Col
+hitBarrier(Maze, Row, Col) :-
+  maze(Maze, Row, Col, Status),
+  Status == barrier.
+
 
 %-----------------------------------
 printCell(Maze, _, Row, Column) :-
-	maze(Maze, Row, Column, barrier), write('x').
+        maze(Maze, Row, Column, barrier), write('x').
 printCell(Maze, _, Row, Column) :-
-	maze(Maze, Row, Column, open), write(' ').
-
+        maze(Maze, Row, Column, open), write(' ').
 
 %----------------------------------
 printMaze(Maze, _) :-
-	printHeader(Maze, 0),nl,
-	mazeSize(Maze, Rows, _),
-	printHeader(Maze, 0).
+        printHeader(Maze, 0),nl,
+        mazeSize(Maze, Rows, _),
+        printHeader(Maze, 0).
 
 %-----------------------------------
-solve(Maze) :- true.
+
 
 %-----------------------------------
 printList([]).
 printList([H|T]) :-
-	write(H),
-	printList(T).
+        write(H),
+        printList(T).
 
 %-----------------------------------
 printHeader(Maze, Col) :-
   Col =:= 0,
-	write('+'),
-	printHeader(Maze, 1).
+        write('+'),
+        printHeader(Maze, 1).
 printHeader(Maze, Col) :-
-	mazeSize(Maze,_,TotalCol),
-	Col < TotalCol,
-	write('-'),
-	Next is Col + 1,
-	printHeader(Maze, Next).
+        mazeSize(Maze,_,TotalCol),
+        Col < TotalCol,
+        write('-'),
+        Next is Col + 1,
+        printHeader(Maze, Next).
 printHeader(Maze, Col) :-
-	mazeSize(Maze,_,TotalCol),
-	Col =:= TotalCol,
-	write('+').
+        mazeSize(Maze,_,TotalCol),
+        Col =:= TotalCol,
+        write('+').
 
 %-----------------------------------
 printRow(Maze, Row, Col) :-
   Col =:= 0,
-	write('|'),
-	printRow(Maze, Row, 1).
+        write('|'),
+        printRow(Maze, Row, 1).
 
 printRow(Maze, Row, Col) :-
-	mazeSize(Maze, _, TotalCol),
-	Col < TotalCol,
-	printCell(Maze, _, Row, Col),
-	NextCol is Col + 1,
-	printRow(Maze, Row, NextCol).
+        mazeSize(Maze, _, TotalCol),
+        Col < TotalCol,
+        printCell(Maze, _, Row, Col),
+        NextCol is Col + 1,
+        printRow(Maze, Row, NextCol).
 
 printRow(Maze, Row, Col) :-
-	mazeSize(Maze, _, TotalCol),
-	Col =:= TotalCol,
-	write('|').
+        mazeSize(Maze, _, TotalCol),
+        Col =:= TotalCol,
+        write('|').
 
 %-----------------------------------
 printAllRows(Maze, Row) :-
   Row =:= 0,
-	printRow(Maze, Row, 0), nl,
-	printAllRows(Maze, 1).
+        printRow(Maze, Row, 0), nl,
+        printAllRows(Maze, 1).
 
 printAllRows(Maze, Row) :-
-	mazeSize(Maze, TotalRows, _),
-  Row =< TotalRows,
-	printRow(Maze, Row, 0), nl,
-	Next is Row + 1,
-	printAllRows(Maze, Next).
+        mazeSize(Maze, TotalRows, _),
+  		Row =< TotalRows,
+        printRow(Maze, Row, 0), nl,
+        Next is Row + 1,
+        printAllRows(Maze, Next).
--- solve.pl
+++ solve.pl
@@ -3,7 +3,27 @@
 % the approach.
 
 % Try a move in an "Up" direction, assumes Row and Column are bound.
-try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column - 1.
+
+% Move up
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row - 1,
+%  NextCol is Col.
+
+% Move down
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row + 1,
+%  NextCol is Col - 1.
+
+% Move right
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row,
+%  NextCol is Col + 1.
+
+% Move left
+%try(Row, Col, NextRow, NextCol) :-
+%  NextRow is Row,
+%  NextCol is Col - 1.
+
 
 % move(Maze, List, NewList, Row, Column, GoalRow, GoalColumn) - moves,
 %   and keep on moving until the GoalRow and GoalColumn coordinates
@@ -11,84 +31,122 @@ try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column - 
 %   that the current move is not one previously made), NewList will be
 %   bound to the list of successful moves in a solved maze.
 
-%
-%   Recursive case still needed.
+% Arrived at goal
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  Row =:= GoalRow,
+  Col =:= GoalColumn,
+  append(List, [[Row,Col]], NewList).
 
-% printCell(Maze, List, Row, Column) - helper goal for printMaze, printCell
-%   prints a single cell in the maze.
-%
-%   Print a barrier.
-printCell(Maze, _, Row, Col) :-
-  maze(Maze, Row, Col, barrier),
-  write('x').
-printCell(Maze, _, Row, Col) :-
-  write(' ').
-
-printRow(Name, Row, 0) :-
-  write('|'),
-  printRow(Name, Row, 1).
-
-printRow(Name, Row, Col) :-
-  mazeSize(Name, R2, C2),
-printRow(Name, Row, Col) :-
-  maze(Name, Row, Col, barrier),
-  write('x').
-printRow(Name, Row, Col) :-
-  maze(Name, Row, Col, path),
-  write('*').
+% Found an open square
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  maze(Maze, Row, Col, Status),
+  Status == open,
+  append(List, [[Row,Col]], AppendedList),
+  NextCol is Col + 1,
+  move(Maze, AppendedList, NewList, Row, NextCol, GoalRow, GoalColumn).
 
 
-%---------------------------------------------------------------------
-printMaze(Maze, List) :-
-  mazeSize(Maze, Row, Col),
-  write('Rows:'), write(Row), nl,
-  write('Cols:'), write(Col), nl,
-  printHeader(Col, Col),
-  printRow(Maze, Row, Col).
+% Hit a barrier, move right
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  C2 is Col + 1,
+  move(Maze, List, NewList, Row, C2, GoalRow, GoalColumn).
 
-%---------------------------------------------------------------------
-solve(Maze) :- true.
+% Hit a barrier, move down
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  R2 is Row + 1,
+  move(Maze, List, NewList, R2, Col, GoalRow, GoalColumn).
 
-%---------------------------------------------------------------------
+% Hit a barrier, move left
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  C2 is Col - 1,
+  move(Maze, List, NewList, Row, C2, GoalRow, GoalColumn).
+
+% Hit a barrier, move up
+move(Maze, List, NewList, Row, Col, GoalRow, GoalColumn) :-
+  hitBarrier(Maze, Row, Col),
+  R2 is Row - 1,
+  move(Maze, List, NewList, R2, Col, GoalRow, GoalColumn).
+
+%----------------------
+solve(Maze) :-
+	mazeSize(Maze, GoalRow, GoalColumn),
+	move(Maze, [], NewList, 1, 1, GoalRow, GoalColumn),
+	write(NewList).
+
+
+% Helper predicate, return true if barrier is at Row, Col
+hitBarrier(Maze, Row, Col) :-
+  maze(Maze, Row, Col, Status),
+  Status == barrier.
+
+
+%-----------------------------------
+printCell(Maze, _, Row, Column) :-
+        maze(Maze, Row, Column, barrier), write('x').
+printCell(Maze, _, Row, Column) :-
+        maze(Maze, Row, Column, open), write(' ').
+
+%----------------------------------
+printMaze(Maze, _) :-
+        printHeader(Maze, 0),nl,
+        mazeSize(Maze, Rows, _),
+        printHeader(Maze, 0).
+
+%-----------------------------------
+
+
+%-----------------------------------
 printList([]).
 printList([H|T]) :-
-  write(H),
-  printList(T).
+        write(H),
+        printList(T).
 
-appendPoint(P1, list):-
-  append(P1, list, Z).
+%-----------------------------------
+printHeader(Maze, Col) :-
+  Col =:= 0,
+        write('+'),
+        printHeader(Maze, 1).
+printHeader(Maze, Col) :-
+        mazeSize(Maze,_,TotalCol),
+        Col < TotalCol,
+        write('-'),
+        Next is Col + 1,
+        printHeader(Maze, Next).
+printHeader(Maze, Col) :-
+        mazeSize(Maze,_,TotalCol),
+        Col =:= TotalCol,
+        write('+').
 
-%---------------------------------------------------------------------
-printHeader2(Maze, 0) :-
-  write('+'),
-  printHeader2(Maze, 1).
-printHeader2(Maze, Col) :-
-  mazeSize(Maze,_,TotalCol),
-  Col < TotalCol,
-  write('-'),
-  Next is Col+1,
-  printHeader2(Maze, Next).
-printHeader2(Maze, Col) :-
-  mazeSize(Maze,_,TotalCol),
-    Col =:= TotalCol,
-    write('+').
+%-----------------------------------
+printRow(Maze, Row, Col) :-
+  Col =:= 0,
+        write('|'),
+        printRow(Maze, Row, 1).
 
-%---------------------------------------------------------------------
-printHeader(0, _) :- write('+'), !.
-printHeader(X, L) :-
-  X =:= L,
-  write('+'),
-  Y is X-1,
-  printHeader(Y, L).
-printHeader(X, L) :-
-  write('-'),
-  Y is X-1,
-  printHeader(Y, L).
+printRow(Maze, Row, Col) :-
+        mazeSize(Maze, _, TotalCol),
+        Col < TotalCol,
+        printCell(Maze, _, Row, Col),
+        NextCol is Col + 1,
+        printRow(Maze, Row, NextCol).
 
-printAllRows(Maze, 0) :-
-  printRow(Maze, 0, 0).
+printRow(Maze, Row, Col) :-
+        mazeSize(Maze, _, TotalCol),
+        Col =:= TotalCol,
+        write('|').
+
+%-----------------------------------
 printAllRows(Maze, Row) :-
-  mazeSize(Maze, TotalRows, _),
-  printRow(Maze, Row, 0),
-  Next is Row + 1,
-  printAllRows(Maze, Next).
+  Row =:= 0,
+        printRow(Maze, Row, 0), nl,
+        printAllRows(Maze, 1).
+
+printAllRows(Maze, Row) :-
+        mazeSize(Maze, TotalRows, _),
+  		Row =< TotalRows,
+        printRow(Maze, Row, 0), nl,
+        Next is Row + 1,
+        printAllRows(Maze, Next).
